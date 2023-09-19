78e183ca4e11b67ea98dfeda69880782759606b7
yngkzk <131941774+yngkzk@users.noreply.github.com>
2023-09-19 21:37:44 +0600

    added ui interface

--- gui/gui.py
+++ gui/gui.py
@@ -1,6 +1,31 @@
-from PyQt6.QtCore import QThread
+from PyQt6.QtCore import QThread, pyqtSignal
+from PyQt6.QtWidgets import *
+from PyQt6 import uic
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("gui/main.ui", self)
 
 
 class Gui(QThread):
-    def run(self):
+    def __init__(self):
+        super().__init__()
+        self.window = MainWindow()
+        self.window.show()
+
+    def start(self):
         print('GUI has been launched!')
+        button = self.window.findChild(QPushButton, 'pushbutton')
+        button.clicked.connect(self.sendMessage)
+
+    def sendMessage(self):
+        print('Button pressed')
+        textedit = self.window.findChild(QTextEdit, 'MessageToSend')
+        message = textedit.text()
+
+
+# if __name__ == '__main__':
+#     gui = Gui()
+#     gui.start()
