b2b348bfe3d9d9c76a604ffaf1e477f24a25837e
ChiriNNi <103880918+ChiriNNi@users.noreply.github.com>
2023-09-25 21:46:13 +0600

    Доработки в GUI
    Переход на match-case

--- gui/gui.py
+++ gui/gui.py
@@ -25,18 +25,18 @@ class GUI(QThread):
         self.running = True 
     
     def set_window(self, window_name, username=None): 
-        if self.window is not None: 
-            self.window.hide() 
+        if self.window is not None: self.window.hide() 
 
-        if window_name == "MainWindow": 
-            self.window = MainWindow(username)
-            self.window.sendMessage.connect(self.loginUser)
-        elif window_name == "LoginWindow": 
-            self.window = LoginWindow()  
-            self.window.loginUser.connect(self.loginUser)
-
-        if self.running:
-            self.run()
+        match window_name: 
+            case "MainWindow": 
+                self.window = MainWindow(username)
+                self.window.sendMessage.connect(self.loginUser)
+            case "LoginWindow":
+                self.window = LoginWindow()  
+                self.window.loginUser.connect(self.loginUser)
+            case _:
+                log.e("Неизвестное имя окна:", window_name)
+        if self.running: self.run()
 
 
 
