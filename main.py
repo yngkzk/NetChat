739f99a45eaaad63c20241859ed9a53efdc3cb97
yngkzk <ansar.iskakow@gmail.com>
2023-09-27 21:37:52 +0600

    added Logger

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from router import Router
+from logger import Logger
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
+    log = Logger(Logger.DEBUG)
     router = Router()
     router.start()
 
